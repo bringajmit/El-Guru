01930e3be0ab625ba79fc6a5dc6c0f40131971df
bringajmit <bringajmit@gmail.com>
2022-06-04 16:52:49 -0300

    correccion de errores

--- js/acciones.js
+++ js/acciones.js
@@ -1,4 +1,4 @@
-var palabras=["si","No","Tal Vez","Nunca","Piensalo mejor","Si yo fuera tu lo pensaria mejor","El tren pasa una vez en la vida, asi que es un rotundo SI","La duda de actuar es un si camuflajeado, te doy un empujon con un Si","Necesito que sientas la pregunta con mas fuerza no logro conectar contigo","No. Pero te dejo una reflexion al respecto, un No solo indica el incio de un nuevo camino no el fin del mismo"];
+var palabras=["si","No","Tal Vez","Nunca","Piensalo mejor","Si yo fuera tu lo pensaria mejor","El tren pasa una vez en la vida, asi que es un rotundo SI","La duda de actuar es un si camuflajeado, te doy un empujon con un Si","Necesito que sientas la pregunta con mas fuerza no logro conectar contigo","No. Pero te dejo una reflexion al respecto, un No solo indica el inicio de un nuevo camino no el fin del mismo"];
 var aleatorio;
 const minimo=0;
 const maximo=palabras.length;
